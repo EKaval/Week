4280f638255585333c7aa8d5d6c4b1ef1d05e972
EKaval <vsacxz@gmail.com>
2025-02-05 18:56:37 +0300

    style: reformated code using

--- hello.world.cpp
+++ hello.world.cpp
@@ -1,6 +1,12 @@
 #include <iostream>
 using namespace std;
 int main() {
-cout « "Hello World!" << endl;
+//A variable for the user's name
+std::string name;
+//We enter the user's name
+std::cout << "Enter your name: " << endl;
+std::cin >> name;
+//We output the greeting and the username
+std::cout << "Hello World from " << name << "!" << endl;
 return 0; 
 }
